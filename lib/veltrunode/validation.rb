28d93ec50ef46168af10e1c7b255f08a788fd03b
hirontan <hironori.t.0405@gmail.com>
2026-07-31 22:50:08 +0900

    feat(model): ValidationErrorにdiagnostics保持機能を追加

--- lib/veltrunode/validation.rb
+++ lib/veltrunode/validation.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 module Veltrunode
-  class ValidationError < Error; end
+  class ValidationError < Error
+    attr_reader :diagnostics
+
+    def initialize(message = nil, diagnostics: [])
+      @diagnostics = diagnostics.dup.freeze
+      super(message)
+    end
+  end
 
   module Validation
   end
