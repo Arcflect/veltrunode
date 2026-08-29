94fd494a14270f5bf32271fbdfa7d6e93171ddd9
hirontan <hironori.t.0405@gmail.com>
2026-08-30 01:30:14 +0900

    feat(compiler): ScheduleモデルからAWS::Scheduler::Scheduleへの変換を行うScheduleCompilerの実装

--- lib/veltrunode/compiler/cloudformation.rb
+++ lib/veltrunode/compiler/cloudformation.rb
@@ -3,6 +3,7 @@
 require_relative 'cloudformation/function_compiler'
 require_relative 'cloudformation/log_group_compiler'
 require_relative 'cloudformation/role_compiler'
+require_relative 'cloudformation/schedule_compiler'
 
 module Veltrunode
   module Compiler
